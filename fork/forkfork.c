e4563672d7e849d65edff205681e751881633220
kamiwie749 <kamiwie749@student.polsl.pl>
2020-10-14 12:02:10 +0200

    Change forkfork.c

--- fork/forkfork.c
+++ fork/forkfork.c
@@ -4,36 +4,55 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-void rekurencja(int ile){
-    printf("ile = %d\n",ile);
+void rekurencja(int ile, int ileTab[], int kursor){
+    int j;
     if(!fork()){
-        printf("Proces potomny PID = %d PPID = %d\n",getpid(),getppid());
-        sleep(1);
+        for(j = 0; j < ileTab[kursor]; j++){
+            printf("Proces potomny PID = %d PPID = %d\n",getpid(),getppid());
+            sleep(1);
+        }
         //exit(ile); //nwm jak dobrze zakończyć te procesy
-        if(ile>0){
+        if(ile>1){
             printf("tak\n");
-            rekurencja(ile-1);
+            rekurencja(ile-1, ileTab, kursor + 1);
         }
     }
 }
 
 
 int main(int argc, char* argv[]){
-    int i, status, kod;
-    
-    //instrukcje procesu macierzystego 
-    printf("Proces macierzysty PID = %d, PPID = %d\n",getpid(), getppid());
-    sleep(1);
-    
-    rekurencja(atoi(argv[1]));
-    
-    for(i=0; i<atoi(argv[1]);i++){
-        kod = wait(&status);
-        printf("Zamknieto proces %d  kodem %d\n",kod, WEXITSTATUS(status));
+    int i, kursor, status, kod, pid, *ileTab;
+    int ile = argc - 2; //- nazwa - maciezysty
+    pid = getpid();
+    ileTab = malloc(ile*sizeof(int));
+    for(i = 0; i < ile; i++){
+        ileTab[i] = atoi(argv[i+2]);
     }
-   //while(1);
+    kursor = 0;
+    
+    rekurencja(ile, ileTab, kursor);
+    
+    if(getpid() == pid){
+        for(i=0; i < atoi(argv[1]); i++){
+            printf("Proces macierzysty PID = %d, PPID = %d\n",getpid(), getppid());
+            sleep(1);
+        }
+    }
+    
+    for(i=2; i<argc ;i++){
+        kod = wait(&status);
+        //printf("Zamknieto proces %d  kodem %d\n",kod, WEXITSTATUS(status));
+    }
+    
    return 0;
 }
+
+
+
+
+
+
+
 
 
 
